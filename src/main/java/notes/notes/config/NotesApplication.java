ed5a9a179168dda072829e97ce0fe2487befc780
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2023-12-31 15:44:11 -0600

    Updated configuration to include entity scanning

--- src/main/java/notes/notes/config/NotesApplication.java
+++ src/main/java/notes/notes/config/NotesApplication.java
@@ -2,8 +2,10 @@ package notes.notes.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("notes.notes.domain")
 public class NotesApplication {
 
     public static void main(String[] args) {

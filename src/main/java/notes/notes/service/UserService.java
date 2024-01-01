87e1f31ffa129c5a82041cb29961087176c591cf
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2023-12-31 20:57:23 -0600

    Implemented create user service

--- src/main/java/notes/notes/service/UserService.java
+++ src/main/java/notes/notes/service/UserService.java
@@ -1,4 +1,30 @@
 package notes.notes.service;
 
+import jakarta.transaction.Transactional;
+import notes.notes.adapter.UserAdapter;
+import notes.notes.domain.User;
+import notes.notes.dto.UserDTO;
+import notes.notes.exception.InvalidArgumentException;
+import notes.notes.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
 public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO createUser(UserDTO userDTO) throws InvalidArgumentException {
+        if (userDTO == null)
+            throw new InvalidArgumentException("UserDTO is null", HttpStatus.BAD_REQUEST);
+
+        User user = userRepository.save(UserAdapter.getUserEntity(userDTO));
+
+        return UserAdapter.getUserDTO(user);
+    }
 }

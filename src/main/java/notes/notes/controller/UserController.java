2f98ea2891ef028baf1fcec09de6cf701e4cd236
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2023-12-31 21:07:05 -0600

    Implemented create user endpoint

--- src/main/java/notes/notes/controller/UserController.java
+++ src/main/java/notes/notes/controller/UserController.java
@@ -1,9 +1,35 @@
 package notes.notes.controller;
 
+import notes.notes.dto.UserDTO;
+import notes.notes.exception.InvalidArgumentException;
+import notes.notes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) throws InvalidArgumentException{
+        if (userDTO == null)
+            throw new InvalidArgumentException("UserDTO is null", HttpStatus.BAD_REQUEST);
+
+        UserDTO newUser = userService.createUser(userDTO);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 }

a79bb2bcbc55c66448d5401622b98156a305df66
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2023-12-31 21:25:06 -0600

    updated create note service to check the existence of a user

--- src/main/java/notes/notes/service/NoteService.java
+++ src/main/java/notes/notes/service/NoteService.java
@@ -6,6 +6,8 @@ import notes.notes.domain.Note;
 import notes.notes.dto.NoteDTO;
 import notes.notes.exception.InvalidArgumentException;
 import notes.notes.repository.NotesRepository;
+import notes.notes.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +16,22 @@ public class NoteService {
 
     private final NotesRepository notesRepository;
 
-    public NoteService(NotesRepository notesRepository) {
+    private final UserRepository userRepository;
+
+    public NoteService(NotesRepository notesRepository, UserRepository userRepository) {
         this.notesRepository = notesRepository;
+        this.userRepository = userRepository;
     }
 
     public NoteDTO create(NoteDTO noteDTO) throws InvalidArgumentException {
         if (noteDTO == null)
             throw new InvalidArgumentException("NoteDTO is null");
+
+        if (noteDTO.getUserId() == null)
+            throw new InvalidArgumentException("User id is null");
+
+        if (!userRepository.existsById(noteDTO.getUserId()))
+            throw new InvalidArgumentException("User with id " + noteDTO.getUserId() + " does not exist", HttpStatus.NOT_FOUND);
 
             Note newNote = notesRepository.save(NoteAdapter.getNoteEntity(noteDTO));
 

bb9e60d266c88d08ab7b9c10101d02f37d2ce47a
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 18:21:31 -0600

    Refactor UserDTO class to include first name, last name, and role

--- src/main/java/notes/notes/dto/UserDTO.java
+++ src/main/java/notes/notes/dto/UserDTO.java
@@ -3,15 +3,16 @@ package notes.notes.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import notes.notes.domain.Role;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserDTO {
     private Integer id;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String password;
-    private String name;
     private String email;
-    private String role;
+    private Role role;
 }

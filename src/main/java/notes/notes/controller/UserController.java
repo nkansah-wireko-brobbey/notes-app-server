02ab8c0e01f27fd52fdc3083581b5c345e3e1f7d
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 18:22:36 -0600

    Add signup and signin endpoints to UserController

--- src/main/java/notes/notes/controller/UserController.java
+++ src/main/java/notes/notes/controller/UserController.java
@@ -1,7 +1,12 @@
 package notes.notes.controller;
 
+import lombok.RequiredArgsConstructor;
+import notes.notes.dto.JwtAuthenticationResponse;
+import notes.notes.dto.SignInRequest;
+import notes.notes.dto.SignUpRequest;
 import notes.notes.dto.UserDTO;
 import notes.notes.exception.InvalidArgumentException;
+import notes.notes.service.AuthenticationService;
 import notes.notes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,16 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
+    private final AuthenticationService authenticationService;
 
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) throws InvalidArgumentException{
@@ -31,5 +32,13 @@ public class UserController {
         UserDTO newUser = userService.createUser(userDTO);
 
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+    @PostMapping("/signup")
+    public JwtAuthenticationResponse signup(@RequestBody SignUpRequest request) throws InvalidArgumentException {
+        return authenticationService.signup(request);
+    }
+    @PostMapping("/signin")
+    public JwtAuthenticationResponse signin(@RequestBody SignInRequest request) {
+        return authenticationService.signin(request);
     }
 }

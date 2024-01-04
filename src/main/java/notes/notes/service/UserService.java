977904371e05a7e6addf9a54227513f8a48d60b6
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-03 18:20:39 -0600

    Add UserDetailsService to UserService

--- src/main/java/notes/notes/service/UserService.java
+++ src/main/java/notes/notes/service/UserService.java
@@ -1,22 +1,33 @@
 package notes.notes.service;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import notes.notes.adapter.UserAdapter;
 import notes.notes.domain.User;
 import notes.notes.dto.UserDTO;
 import notes.notes.exception.InvalidArgumentException;
 import notes.notes.repository.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Transactional
+@RequiredArgsConstructor
 @Service
 public class UserService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
     }
 
     public UserDTO createUser(UserDTO userDTO) throws InvalidArgumentException {

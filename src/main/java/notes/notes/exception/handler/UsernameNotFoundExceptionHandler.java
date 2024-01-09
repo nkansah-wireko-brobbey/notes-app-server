b815b847d53a32706c9672106e3d1eb86722002b
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:36:00 -0600

    Implemented login exception handler

---
+++ src/main/java/notes/notes/exception/handler/UsernameNotFoundExceptionHandler.java
@@ -0,0 +1,17 @@
+package notes.notes.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class UsernameNotFoundExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+}

b245a2b4c2bf6c9f745c50a7f2c8470132df6548
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:32:55 -0600

    Updated test endpoint to seriablizable

--- src/main/java/notes/notes/controller/TestController.java
+++ src/main/java/notes/notes/controller/TestController.java
@@ -1,13 +1,19 @@
 package notes.notes.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
 
     @RequestMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<>("Hello Word", HttpStatus.OK);
+    }
     public String hello() {
         return "This is a test";
     }

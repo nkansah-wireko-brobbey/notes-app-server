8d67317799807ac5eb630f44ff4db35c1659cc3e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-09 16:47:54 -0600

    Added optional import

--- src/main/java/notes/notes/service/UserService.java
+++ src/main/java/notes/notes/service/UserService.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Transactional
 @RequiredArgsConstructor
 @Service

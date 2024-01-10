50102a38e898126dc326d95292752c405da96727
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-01-10 09:13:18 -0600

    Updated domains to dbms managed generation strategy

--- src/main/java/notes/notes/domain/Note.java
+++ src/main/java/notes/notes/domain/Note.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Note {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String title;
--- src/main/java/notes/notes/domain/User.java
+++ src/main/java/notes/notes/domain/User.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
 
